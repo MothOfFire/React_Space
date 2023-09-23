21659e93284b4108990c82cc3fa6921ce10eeb95
MothOfFire <1748972220@qq.com>
2023-09-23 22:12:54 +0800

    feat:添加了sass依赖并创建了全局样式

--- vite-React-TS-antd/management-react/src/main.tsx
+++ vite-React-TS-antd/management-react/src/main.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+//样式初始化一般放在最前面
 import "reset-css"
+//UI框架样式
+
+//全局样式
+import './assets/styles/global.scss'
+
+//组件的样式
+import App from './App.tsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
