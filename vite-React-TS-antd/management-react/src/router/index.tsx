5ee78c6a1a7a5432389fb9c26551ce87a4ca4908
MothOfFire <1748972220@qq.com>
2023-09-24 19:11:31 +0800

    feat:嵌套路由和侧边栏的路由跳转的实现

--- vite-React-TS-antd/management-react/src/router/index.tsx
+++ vite-React-TS-antd/management-react/src/router/index.tsx
@@ -6,8 +6,10 @@ import Home from '../views/Home';
 // import User from '../views/User';
 import { Navigate } from 'react-router-dom';
 
-const About = lazy(() => import('../views/About'));
-const User = lazy(() => import('../views/User'));
+// const About = lazy(() => import('../views/About'));
+// const User = lazy(() => import('../views/User'));
+const Page1 = lazy(() => import('../views/page1'));
+const Page2 = lazy(() => import('../views/page2'));
 
 //懒加载模式的组件写法，外面需要套一层 Loading 的提示加载组件
 
@@ -22,20 +24,35 @@ const withLoadingComponent = (comp: JSX.Element) => {
 const routes = [
     {
         path: "/",
-        element: <Navigate to='/home' />
+        element: <Navigate to='/page1' />
     },
+    //嵌套路由
     {
-        path: "/home",
-        element: <Home />
-    },
-    {
-        path: "/about",
-        element: withLoadingComponent(<About />)
-    },
-    {
-        path: "/user",
-        element: withLoadingComponent(<User />)
+        path: "/",
+        element: <Home />,
+        children: [
+            {
+                path: "/page1",
+                element: withLoadingComponent(<Page1 />)
+            },
+            {
+                path: "/page2",
+                element: withLoadingComponent(<Page2 />)
+            }
+        ]
     }
+    // {
+    //     path: "/home",
+    //     element: <Home />
+    // },
+    // {
+    //     path: "/about",
+    //     element: withLoadingComponent(<About />)
+    // },
+    // {
+    //     path: "/user",
+    //     element: withLoadingComponent(<User />)
+    // }
 ]
 
 export default routes;
--- vite-React-TS-antd/management-react/src/views/Home.tsx
+++ vite-React-TS-antd/management-react/src/views/Home.tsx
@@ -8,6 +8,7 @@ import {
 } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
 import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -28,8 +29,8 @@ function getItem(
 }
 
 const items: MenuItem[] = [
-  getItem('Option 1', '1', <PieChartOutlined />),
-  getItem('Option 2', '2', <DesktopOutlined />),
+  getItem('Option 1', '/page1', <PieChartOutlined />),
+  getItem('Option 2', '/page2', <DesktopOutlined />),
   getItem('User', 'sub1', <UserOutlined />, [
     getItem('Tom', '3'),
     getItem('Bill', '4'),
@@ -41,16 +42,24 @@ const items: MenuItem[] = [
 
 const View: React.FC = () => {
   const [collapsed, setCollapsed] = useState(false);
+  const navigateTo = useNavigate();
   const {
     token: { colorBgContainer },
   } = theme.useToken();
+
+//   侧边栏的点击事件
+  const menuClick = (e: {key: string}) => {
+    console.log('点击了菜单', e.key);
+    // 点击跳转到对应的路由  编程式导航跳转 利用到一个hook
+    navigateTo(e.key); //e.key 是 items 的 getItem 的第二个参数的值
+  }
 
   return (
     <Layout style={{ minHeight: '100vh' }}>
         {/* 左边侧边栏 */}
       <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
         <div className="demo-logo-vertical" />
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} onClick={menuClick} />
       </Sider>
       {/* 右边内容区域 */}
       <Layout>
@@ -65,6 +74,7 @@ const View: React.FC = () => {
         {/* 内容区域 */}
         <Content style={{ margin: '16px 16px 0',backgroundColor: 'red' }} >
           {/* 窗口部分 */}
+          <Outlet />
         </Content>
         {/* 底部 */}
         <Footer style={{ textAlign: 'center', padding: 0, lineHeight: '48px' }}>Ant Design ©2023 Created by Ant UED</Footer>
