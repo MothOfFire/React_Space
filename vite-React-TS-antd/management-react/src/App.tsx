b9c6e90ffa534b7ee8ec4d1e339687e083f40e66
MothOfFire <1748972220@qq.com>
2023-09-24 18:37:30 +0800

    feat:添加了首页的初始模板

--- vite-React-TS-antd/management-react/src/App.tsx
+++ vite-React-TS-antd/management-react/src/App.tsx
@@ -1,5 +1,5 @@
 // import { Outlet, Link } from 'react-router-dom';
-import { useRoutes, Link } from 'react-router-dom';
+import { useRoutes } from 'react-router-dom';
 import router from './router';
 
 function App() {
@@ -11,9 +11,9 @@ function App() {
       {/* 顶级组件
       <Button type="primary">按钮</Button>
       <FullscreenOutlined style={{ fontSize: '40px'}} /> */}
-      <Link to="/home">Home</Link> |
+      {/* <Link to="/home">Home</Link> |
       <Link to="/about">About</Link> |
-      <Link to="/user">User</Link>
+      <Link to="/user">User</Link> */}
       {/* <Outlet></Outlet> */}
       { outlet }
     </div>
